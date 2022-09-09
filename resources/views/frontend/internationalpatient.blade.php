8f5759b3dcd0fd3f50193bbd5ca634880cb803bf
fridoon <fnajmullah@yahoo.com>
2022-09-09 11:21:39 +0300

    added int patient blade

---
+++ resources/views/frontend/internationalpatient.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>International Patient</h5>
+                </div>
+                <div class="card-body">
+                    <p>Coming Soon...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
